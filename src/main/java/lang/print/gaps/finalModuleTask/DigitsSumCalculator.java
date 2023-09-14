2cafd56c2c1cfa5cfa3f372e377dedc926ca26fd
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-14 13:59:43 +0400

    m3TF

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int thousands = number / 1000;
+        int hundreds = (number % 1000) / 100;
+        int tens = (number % 100) / 10;
+        int ones = number % 10;
+
+
+        int sum = thousands + hundreds + tens + ones;
+
+        System.out.println(sum);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
+    public static void main(String[] args) {
+        NumberReverter nr = new NumberReverter();
+        nr.revert(489);
+    }
+
     public void revert(int number) {
+        String numberStr = Integer.toString(number);
+        String reversedStr = new StringBuilder(numberStr).reverse().toString();
+        int reversedNumber = Integer.parseInt(reversedStr);
+        System.out.println(reversedNumber);
     }
 }
